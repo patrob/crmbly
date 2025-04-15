dd76e58ab3ec50023305e3938acea6fcaa07e53f
Patrick Robinson <patrob@gmail.com>
2025-04-15 09:16:59 -0500

    added pr-verify action and terraform code for deploying. hope this works first try

---
+++ terraform/main.tf
@@ -0,0 +1,36 @@
+provider "azurerm" {
+  features {}
+  # Use the environment variables set by the azure/login action
+  use_cli = false
+}
+
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.2.0"
+    }
+  }
+
+  backend "azurerm" {
+    resource_group_name  = "crmbly-tfstate"
+    storage_account_name = "crmblytfstate"
+    container_name       = "prod-tfstate"
+    use_azuread_auth     = true
+  }
+}
+
+locals {
+  resource_prefix = "crmbly"
+  common_tags = {
+    Environment = var.environment
+    Project     = "Crmbly"
+    ManagedBy   = "Terraform"
+  }
+}
+
+resource "azurerm_resource_group" "main" {
+  name     = "rg-${local.resource_prefix}-${var.environment}"
+  location = "eastus"
+  tags     = local.common_tags
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,5 @@
+variable "environment" {
+  description = "The environment"
+  type        = string
+  default     = "prod"
+}
